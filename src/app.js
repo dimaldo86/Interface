7c31e9da4c5a7da588b704c70c4bb73faab60833
Dmitry Menshikov <dimaldo86ne@mail.ru>
2021-09-19 02:57:09 +0700

    validation form

--- src/app.js
+++ src/app.js
@@ -10,3 +10,27 @@
         header.classList.remove('fixed-top');
     }   
 }) ; 
+
+(function () {
+    'use strict'
+  
+    // Получите все формы, к которым мы хотим применить пользовательские стили проверки Bootstrap
+    const forms = document.querySelectorAll('.needs-validation');
+  
+    // Зацикливайтесь на них и предотвращайте отправку
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
+//   mask phone
+
